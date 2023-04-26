e2b1ea44eb2e3c3eb62fae3cb7d5e01189b30a4f
rakesh singh <rakesh.doon@gmail.com>
2023-04-26 17:05:36 +0530

    working design 3 track
    working design 3 track

--- js/script.js
+++ js/script.js
@@ -1,71 +1,87 @@
+var design=1; // default value for design
 
-var mm = 3;
+		function setDesign(value) {
+			design = value; // set the value of design based on the button clicked
+      document.getElementById("num1").value = "";
+			document.getElementById("num2").value = "";
+      document.getElementById("fc").value = "";
+      document.getElementById("gifc").value="";
+      document.getElementById("sc").value="";
+      document.getElementById("gisc").value="";
+      document.getElementById("gc").value="";
+      document.getElementById("psf").value="";
+    }
 
-function sum() {
+
+
+function calculate() {
 
   // Get the values of the two textboxes
   var num1 = parseInt(document.getElementById("num1").value);
   var num2 = parseInt(document.getElementById("num2").value);
+  var misc=Math.min(num1*num2/92903.04*50,2000);
 
   // Calculate the sum and display it in the result textbox
+  var fc,gifc,sc,gc,glasscost,beading = 0;
+switch (design) {
+	case 1:
   var fc = document.getElementById("fc");
-  var framecost = (((num1 + num2) * 2 / 19.3) * 5.8) * 269.01 * 1.2;
+  var framecost = ((num1 + num2) * 2 / 1000) * 269.01 * 1.1;
 
   var gifc = document.getElementById("gifc");
-  var giframecost = (((num1 + num2) * 2 / 15.5) * 5 * 41.448) * 1.2;
+  var giframecost = ((num1 + num2) * 2 /1000 * 41.448)*2 * 1.1;
 
   var sc = document.getElementById("sc");
-  var sashcost = (((num1 + (num2 / 2)) * 2 / 19.3) * 5.8 * 182.868 * 3) * 1.2;
+  var sashcost = (((num1)/2 + num2 ) * 2 /1000 * 182.868 * 3) * 1.1;
 
 
   var gisc = document.getElementById("gisc");
-  var gisashcost = (((num1 + (num2 / 2)) * 2 / 15.5 * 5 * 57.272) * 3) * 1.2;
+  var gisashcost = ((((num1/2) + num2) * 2 / 1000 * 57.272) * 3) * 1.1;
 
   var gc = document.getElementById("gc");
-  var glasscost = (num1 * num2) * 80;
-  var beading = (num1 + (num2 / 2)) * 2 / 19.8 * 5.8 * 33.369 * 3;
+  var glasscost = (num1 * num2)/92903.04 * 80;
+  var beading = (num1 + (num2 / 2)) * 2 / 1000 * 33.369 * 3;
 
-  var mesh = (num1 * (num2 / 2)) * 20;
+  var mesh = ((num1/2) * num2 )/92903.04 * 20;
 
   var psf = document.getElementById("psf");
-  var psfcost = framecost + giframecost + sashcost + gisashcost + glasscost + 255 + 170 + 240 + 100 + beading + mesh + 100 + 2000;
+  var psfcost = framecost + giframecost + sashcost + gisashcost + glasscost + 255 + 170 + 240 + 100 + beading + mesh + 100 + misc;
+  
+  console.log(psf);
+  break;
+
+  case 2:
+  var fc = document.getElementById("fc");
+  var framecost = ((num1 + num2) * 2 / 1000) * 42.336 * 1.1;
+
+  var gifc = document.getElementById("gifc");
+  var giframecost = ((num1 + num2) * 2 /1000 * 57.272) * 1.1;
+
+  var sc = document.getElementById("sc");
+  var sashcost = ((num1 + num2)) * 2 /1000 * 180.957 * 1.1;
+
+  var gisc = document.getElementById("gisc");
+  var gisashcost = ((num1 + num2 ) * 2 / 1000 * 54.256) *  1.1;
+
+  var gc = document.getElementById("gc");
+  var glasscost = (num1 * num2)/92903.04 * 80;
+  var beading = (num1 + (num2 / 2)) * 2 / 1000 * 33.369 * 3;
+
+  var mesh = ((num1/2) * num2 )/92903.04 * 20;
+
+  var psf = document.getElementById("psf");
+  var psfcost = framecost + giframecost + sashcost + gisashcost + glasscost + 80 + 100 + beading + mesh + 100 + misc;
+  break;
+  default:
+    console.log("Invalid design selected");
+}
 
   fc.value = framecost;
   gifc.value = giframecost;
   sc.value = sashcost;
   gisc.value = gisashcost;
   gc.value = glasscost;
-  psf.value = psfcost / (num1 * num2)
-}
-
-function singleTrack() {
-  toggle();
-  var mm = 1;
-  console.log(mm);
-}
-
-function casement() {
-  toggle();
-  var mm = 2;
-  console.log(mm);
-}
-
-function threetrack() {
-  toggle();
-  var mm = 3;
-  console.log(mm);
-}
-
-function toggle() {
-  var hideelement = document.getElementById('sccost');
-
-  if (hideelement.style.display === "none") {
-    hideelement.style.display = 'block';
-  }
-  else {
-    // clock is visible. hide it
-    hideelement.style.display = 'none';
-  }
+  psf.value = psfcost / (num1 * num2)*92903.04;
 
 }
 
